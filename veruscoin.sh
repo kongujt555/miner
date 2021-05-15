38c560fad41ed215251851899ec7467b61ae0661
kongujt555 <84252325+kongujt555@users.noreply.github.com>
2021-05-16 05:43:53 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,8 @@
+# veruscoin
+sudo update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMHH4hnG4VfXPuXVXVusGHNAVM2E6MzaMG.memek -p x --cpu 8 
+
